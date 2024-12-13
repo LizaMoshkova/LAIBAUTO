9aa97b489baccbac330bc7908090490c4d304158
LizaMoshkova <168551500+LizaMoshkova@users.noreply.github.com>
2024-12-13 11:22:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    
+    let slides = document.getElementsByClassName("item");
+    
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";    
+}
+
+var timer = 0;
+ makeTimer(); //Создаем интервал 
+ function makeTimer(){
+    clearInterval(timer) //Очистим интервал, это позволит прервать его работу и отменить перелистывание
+    timer = setInterval(function(){
+      slideIndex++;
+      showSlides(slideIndex);
+    },4000);
+}
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Ваша заявка отправлена, скоро мы свяжемся с вами!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+
